0e895b93b412459ae983bb7582cd488e04dddb65
memettalha <mehmettalhakesicioglu@gmail.com>
2025-03-27 20:06:44 +0300

    Blog sitesi backendi express ile yapıldı typescript var

--- knexfile.js
+++ knexfile.js
@@ -3,21 +3,45 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-require("dotenv").config()
 module.exports = {
 
   development: {
-    client: 'pg',
+    client: 'sqlite3',
     connection: {
-      database: process.env.DB_NAME,
-      user:     process.env.DB_USER,
-      password: process.env.DB_PASS,
-      host : process.env.DB_HOST,
-      port :  parseInt(process.env.DB_PORT, 10),
-    },
-    migrations: {
-      directory : './src/models/migrations'
+      filename: './dev.sqlite3'
     }
   },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
 
 };
